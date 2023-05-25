143c3a465ab87a2aff08ab076c3705ee33d95b2a
Daffabot <daffabot@programmer.net>
2023-05-25 11:47:32 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,46 @@
+var accessButton = document.getElementById("access-button");
+  
+  accessButton.addEventListener("click", function() {
+  var confirmation = confirm("Apakah Anda yakin ingin mengakses dokumen?");
+  
+  if (confirmation) {
+  // Kode akses dokumen
+  alert("Anda telah mengakses game.");
+  window.location.href = "./main/start.html";
+  } else {
+  alert("Akses game dibatalkan.");
+  }
+  });
+  
+ var splashScreen = document.getElementById('splash-screen');
+ splashScreen.addEventListener('click',()=>{
+ splashScreen.classList.add("fadeOut");
+ setTimeout(()=>{
+ // Memberikan hash fragment ke URL
+ window.location.hash = "#splash-screen";
+ },610)
+ })
+ window.addEventListener("DOMContentLoaded", function() {
+ // Mendapatkan hash fragment saat ini dari URL
+ var currentHash = window.location.hash;
+ 
+ // Melakukan pengecekan atau tindakan lain berdasarkan hash fragment saat ini
+ if (currentHash === "#splash-screen") {
+ document.getElementById("content").style.display = "block";
+ 
+ } else {
+ document.getElementById("content").style.display = "none";
+ }
+ });
+ window.addEventListener("hashchange", function() {
+ // Mendapatkan hash fragment saat ini dari URL
+ var currentHash = window.location.hash;
+ 
+ // Melakukan pengecekan atau tindakan lain berdasarkan hash fragment saat ini
+ if (currentHash === "#splash-screen") {
+ document.getElementById("content").style.display = "block";
+ 
+ } else {
+ document.getElementById("content").style.display = "none";
+ }
+ });

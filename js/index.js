6b23d169cbdacf8b00574f2ef7c708c317795a3a
Daffabot <daffabot@programmer.net>
2024-02-28 09:17:20 +0700

    [confirm button] Fix : im delete it bro

--- js/index.js
+++ js/index.js
@@ -1,45 +1,36 @@
-var accessButton = document.getElementById("access-button");
-  
-  accessButton.addEventListener("click", function() {
-  var confirmation = confirm("Apakah Anda yakin ingin mengakses game?");
-  
-  if (confirmation) {
-  // Kode akses dokumen
-  window.location.href = "./main/start.html";
+let accessButton = document.getElementById("access-button");
+
+accessButton.addEventListener("click", function () {
+    window.location.href = "./main/start.html";
+});
+
+let splashScreen = document.getElementById("splash-screen");
+splashScreen.addEventListener("click", () => {
+  splashScreen.classList.add("fadeOut");
+  setTimeout(() => {
+    // Memberikan hash fragment ke URL
+    window.location.hash = "#splash-screen";
+  }, 610);
+});
+window.addEventListener("DOMContentLoaded", function () {
+  // Mendapatkan hash fragment saat ini dari URL
+  let currentHash = window.location.hash;
+
+  // Melakukan pengecekan atau tindakan lain berdasarkan hash fragment saat ini
+  if (currentHash === "#splash-screen") {
+    document.getElementById("content").style.display = "block";
   } else {
-  alert("Akses game dibatalkan.");
+    document.getElementById("content").style.display = "none";
   }
-  });
-  
- var splashScreen = document.getElementById('splash-screen');
- splashScreen.addEventListener('click',()=>{
- splashScreen.classList.add("fadeOut");
- setTimeout(()=>{
- // Memberikan hash fragment ke URL
- window.location.hash = "#splash-screen";
- },610)
- })
- window.addEventListener("DOMContentLoaded", function() {
- // Mendapatkan hash fragment saat ini dari URL
- var currentHash = window.location.hash;
- 
- // Melakukan pengecekan atau tindakan lain berdasarkan hash fragment saat ini
- if (currentHash === "#splash-screen") {
- document.getElementById("content").style.display = "block";
- 
- } else {
- document.getElementById("content").style.display = "none";
- }
- });
- window.addEventListener("hashchange", function() {
- // Mendapatkan hash fragment saat ini dari URL
- var currentHash = window.location.hash;
- 
- // Melakukan pengecekan atau tindakan lain berdasarkan hash fragment saat ini
- if (currentHash === "#splash-screen") {
- document.getElementById("content").style.display = "block";
- 
- } else {
- document.getElementById("content").style.display = "none";
- }
- });
+});
+window.addEventListener("hashchange", function () {
+  // Mendapatkan hash fragment saat ini dari URL
+  let currentHash = window.location.hash;
+
+  // Melakukan pengecekan atau tindakan lain berdasarkan hash fragment saat ini
+  if (currentHash === "#splash-screen") {
+    document.getElementById("content").style.display = "block";
+  } else {
+    document.getElementById("content").style.display = "none";
+  }
+});
